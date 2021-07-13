3d41252208cb1b8e38f124bb4450bbf4d024c754
Jan <j-maly@gmx.de>
2021-07-13 21:19:08 +0200

    unfinished-state

---
+++ evoks/tests/models/test_profile.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+
+from django.contrib.auth.models import User
+from Profile.models import Profile
+
+
+class ProfileTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        User.objects.create(username='jhon', password='ok')
+
+    def test_verified_false(self):
+        user = User.objects.get(id=1)
+        self.assertFalse(user.profile.verified)
+
+    def test_name(self):
+        user = User.objects.get(id=1)
+        user.profile.name = 'tom'
+        self.assertEquals(user.profile.name, 'tom')
+
+    def test_verified_true(self):
+        user = User.objects.get(id=1)
+        user.profile.verify()
+        self.assertTrue(user.profile.verified)
