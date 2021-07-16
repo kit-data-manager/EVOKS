9697851368f0867ed89bc1df49086c5bdb4bcb47
evoksontologe <84095208+evoksontologe@users.noreply.github.com>
2021-07-16 16:53:56 +0200

    Placeholders and attributes

--- evoks/vocabularies/models.py
+++ evoks/vocabularies/models.py
@@ -13,22 +13,34 @@ class State(enum.Enum):
 class Vocabulary(models.Model):
     description = models.CharField(max_length=30)
     term_count = models.IntegerField()
+    name = models.CharField(max_length=30, default='')
+
     #on delete flage wrshl entweder SET_NULL oder SET_DEFAULT
     profiles = models.ManyToManyField(Profile, on_delete=models.SET_NULL)
     groups = models.ManyToManyField(GroupProfile, on_delete=models.SET_NULL)
     #many-to-one-fields belong in the 'one' models
     state = State
 
-    class Meta:
-        permissions = (('ownwer', 'Owner'),
-                       ('participant', 'Participant'),
-                       ('spectator', 'Spectator'))
+    #class Meta:
+        #permissions = (('ownwer', 'Owner'),
+                       #('participant', 'Participant'),
+                       #('spectator', 'Spectator'))
 
-    def __init__(self, title):
-        #set title
-        #save user as creator
+    def __init__(self, name : str, creator : Profile) -> None:
+        self.name = name
+
+        self.profiles.add(creator)
+        #missing: save creator in creator field
         #give user owner permissions
-        #state = dev
+        
+
+        self.state = 1
+
+        #fuseki create_vocabulary?
+    
+    def test_if_live_vocabulary(self):
+        if self.state is 3:
+            self.state = 2
     
     def import_vocabulary(input):
         #stub
@@ -37,32 +49,42 @@ class Vocabulary(models.Model):
     
     def export_vocabulary(dataformat):
         #stub
+        placeholder='123'
     
     def set_live():
-        #stub
+        state = 3
+        #migration
     
     def set_review():
-        #stub
+        state = 2
+        #sumn else?
     
     def set_dev():
-        #stub
+        state = 1
     
-    def add_term(name):
-        #stub
+    #def add_term(self, name : str):
+        #Term(self, name : str)
     
-    def add_profile(profile : Profile, permission):
-        #stub
+    def add_profile(self, profile : Profile, permission : str):
+        self.profiles.add(profile)
+        #todo permissions
     
-    def add_group(group : GroupProfile, permission):
-        #stub
+    def add_group(self, group : GroupProfile, permission : str):
+        self.groups.add(group)
 
-    def remove_term(name):
-        #stub
+    #def remove_term(name):
+        term = self.terms.objects.filter(name=name)
+        self.terms.remove(term)
+        term.delete()
+        test_if_live_vocabulary(self)
     
-    def remove_profile(profile : Profile):
-        #stub
+    def remove_profile(self, profile : Profile):
+        self.profiles.remove(profile)
+        #remove permissions
     
-    def remove_group(group : GroupProfile):
-        #stub
+    def remove_group(self, group : GroupProfile):
+        self.groups.remove(group)
+        #remove permissions
 
-
+    def get_name(self):
+        return self.name
