079fa80fe96d8da6ad0395a623e1310e16ab7a76
Jan <j-maly@gmx.de>
2021-07-16 18:07:34 +0200

    group_owner-modified

--- evoks/GroupProfile/models.py
+++ evoks/GroupProfile/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import Group
+from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -7,39 +8,43 @@ from django.dispatch import receiver
 class GroupProfile(models.Model):
     group = models.OneToOneField(Group, on_delete=models.CASCADE)
     description = models.TextField(max_length=500, blank=True)
-    group_owner = models.IntegerField(default=1)
+    group_owner = User
     size = models.IntegerField(default=1)
 
-    """
-    creates Profile when User is created. use: User.objects.create()
-    """
+    
     @receiver(post_save, sender=Group)
     def create_groupprofile(sender, instance, created, **kwargs):
+        """
+        creates Profile when User is created. use: User.objects.create()
+        """
         if created:
             GroupProfile.objects.create(group=instance)
 
-    """
-    saves when matching user saves. use: user.save()
-    """
+    
     @receiver(post_save, sender=Group)
     def save_groupprofile(sender, instance, **kwargs):
+        """
+        saves when matching user saves. use: user.save()
+        """
         instance.groupprofile.save()
 
-    """
-    add_user adds a user to the group and counts groupsize up.
-    """
+    
 
     def add_user(self, User):
+        """
+        add_user adds a user to the group and counts groupsize up.
+        """
         self.group.user_set.add(User)
         self.size = self.size + 1
         self.group.save()
 
-    """ 
-    remove_user removes the given user and counts groupsize down.
-    deletes the group if no user remains.
-    """
+    
 
     def remove_user(self, User):
+        """ 
+        remove_user removes the given user and counts groupsize down.
+        deletes the group if no user remains.
+        """
         self.group.user_set.remove(User)
         self.size = self.size - 1  
         self.group.save()     
