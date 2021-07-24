f3acf704bf567826c68f75fdd43ce9c8620a3f83
evoksontologe <84095208+evoksontologe@users.noreply.github.com>
2021-07-24 20:59:44 +0200

    ...

--- evoks/vocabularies/views.py
+++ evoks/vocabularies/views.py
@@ -35,8 +35,6 @@ def settings(request, name):
     context = {
         'vocabulary': vocabulary
     }
-    if(request.GET.get('terms')):
-        return redirect('vocabulary_terms', name=vocabulary.name)
     if(request.method == 'POST'):
         if('overview' in request.POST):
             return redirect('vocabulary_overview', name=vocabulary.name)
@@ -46,28 +44,34 @@ def settings(request, name):
             return redirect('vocabulary_members', name=vocabulary.name)
         elif('settings' in request.POST):
             return redirect('vocabulary_settings', name=vocabulary.name)
+        elif('delete' in request.POST):
+            #vocabulary.delete()
+            return redirect('profile')
         elif(request.POST['vocabulary-setting'] == 'Live'):
             vocabulary.set_live()
         elif(request.POST['vocabulary-setting'] == 'Development'):
             vocabulary.set_review()
         elif(request.POST['vocabulary-setting'] == 'Private to you'):
             vocabulary.set_dev()
-        if('delete' in request.POST):
-            #vocabulary.delete()
-            return redirect('profile')
+        
     return render(request, 'vocabulary_setting.html', context)
 
 
 def members(request : HttpRequest, name):
-    #vocabulary = Vocabulary.objects.get(name=name)
+    vocabulary = Vocabulary.objects.get(name=name)
     
-    #user = User.objects.create(username='jhon', password='ok',
-                            #email='someone@example.com')
+    user = User.objects.create(username='jhon', password='ok',
+                            email='someone@example.com')
     #vocabulary = Vocabulary.create(name)
     #context ={}
  
     # add the dictionary during initialization
-    #context["members"] = vocabulary.profiles.objects.all()
-    #if(request.GET.get('kick')):
-        #vocabulary.remove_profile(...)
-    return render(request, 'vocabulary_members.html')
+    context = {
+        'vocabulary' : vocabulary,
+        'members' : vocabulary.profiles
+    }
+    if(request.method == 'POST'):
+        if(request.POST['kick'] == 'abc'):
+            vocabulary.remove_profile(vocabulary.profiles.get(username='abc'))
+
+    return render(request, 'vocabulary_members.html', context)
