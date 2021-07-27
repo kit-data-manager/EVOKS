7708491c3e58eb92989de5aa804b61fb709833c7
Jonas Scholz <jonas.scholz@bbscholz.de>
2021-07-27 15:51:19 +0200

    improve vocabulary settings view

--- evoks/vocabularies/models.py
+++ evoks/vocabularies/models.py
@@ -10,15 +10,16 @@ from django.core.mail import EmailMessage
 from django.http import HttpRequest
 
 
-class State(enum.Enum):
+class State(models.TextChoices):
     """State enum that represents the state of the Vocabulary
 
     Args:
         enum: Python Enum
     """
-    DEV = 1
-    REVIEW = 2
-    LIVE = 3
+    DEV = 'Development'
+    REVIEW = 'Review'
+    LIVE = 'Live'
+
 
 class Dataformat(enum.Enum):
     """Dataformat enum represents the different kinds of Format that are selectable when downloading a Vocabulary
@@ -30,8 +31,10 @@ class Dataformat(enum.Enum):
     JSONLD = 2
     TURTLE = 3
 
-#missing triple and searchable Interface TODO
-#TODO delete vocabulary?
+# missing triple and searchable Interface TODO
+# TODO delete vocabulary?
+
+
 class Vocabulary(models.Model):
     name = models.CharField(max_length=30, unique=True)
     profiles = models.ManyToManyField(Profile, blank=True)
@@ -39,8 +42,12 @@ class Vocabulary(models.Model):
     term_count = models.IntegerField(default=0)
     groups = models.ManyToManyField(GroupProfile, blank=True)
 
-    #many-to-one-fields belong in the 'one' models
-    state = State
+    # many-to-one-fields belong in the 'one' models
+    state = models.CharField(
+        choices=State.choices,
+        default=State.DEV,
+        max_length=30
+    )
 
     class Meta:
         permissions = [
@@ -50,19 +57,18 @@ class Vocabulary(models.Model):
         ]
 
     @classmethod
-    def create(cls, name : str, creator : Profile):
-        #TODO return type
-        #TODO Save creator in triple field
-        #TODO fuseki create vocabulary
+    def create(cls, name: str, creator: Profile):
+        # TODO return type
+        # TODO Save creator in triple field
+        # TODO fuseki create vocabulary
         vocabulary = cls(name=name)
         vocabulary.save()
         creator.user.save()
         vocabulary.profiles.add(creator)
         assign_perm('owner', creator.user, vocabulary)
-        vocabulary.state = State.DEV
         vocabulary.save()
         #fuseki_dev = Fuseki.objects.filter(port=3030)
-        #fuseki_dev.create_vocabulary(vocabulary)
+        # fuseki_dev.create_vocabulary(vocabulary)
         return vocabulary
 
     def get_name(self) -> str:
@@ -79,26 +85,27 @@ class Vocabulary(models.Model):
         if self.state is State.LIVE:
             self.set_dev()
 
-    def import_vocabulary(input : MultiValueDict) -> None:
+    def import_vocabulary(input: MultiValueDict) -> None:
         """Imports a Vocabulary
 
         Args:
             input ([type]): Vocabulary to import
         """
-        #mögliche dateiformate: rdf/xml, Json-Ld, Turtle
+        # mögliche dateiformate: rdf/xml, Json-Ld, Turtle
         placeholder = 'sdf'
 
-    def export_vocabulary(self, dataformat : Dataformat) -> None:
+    def export_vocabulary(self, dataformat: Dataformat) -> None:
         """Sends the Vocabulary in the provided dataformat to the users email
 
         Args:
             dataformat (Enum): Desired dataformat
         """
-        #TODO turn vocabulary into file in given format
-        #TODO
-        #TODO either has to take user as argument or sending email part handled in view
+        # TODO turn vocabulary into file in given format
+        # TODO
+        # TODO either has to take user as argument or sending email part handled in view
         vocab = 'placeholder'
-        subject = '{0} Vocabulary in the {1} dataformat'.format(self.name, dataformat)
+        subject = '{0} Vocabulary in the {1} dataformat'.format(
+            self.name, dataformat)
         body = 'Attached to this email is the requested Vocabulary...'
         email = EmailMessage(
             subject,
@@ -113,37 +120,33 @@ class Vocabulary(models.Model):
     def set_live(self) -> None:
         """Sets the state to live and starts the migration process
         """
-        if self.state == State.LIVE:
-            raise ValueError('Vocabulary is already live')
-        else:
+        if self.state != State.LIVE:
             self.state = State.LIVE
-            #thread?
-            #fuseki.startvocabularycopy...
-            #versionsnummer?
-            #migration
-            #skosmos
+            # thread?
+            # fuseki.startvocabularycopy...
+            # versionsnummer?
+            # migration
+            # skosmos
 
     def set_review(self) -> None:
         """Sets the state to review
         """
-        if self.state == State.REVIEW:
-            raise ValueError('Vocabulary is already in review')
-        self.state = State.REVIEW
-        #add to skosmos_dev
+        if self.state != State.REVIEW:
+            self.state = State.REVIEW
+        # add to skosmos_dev
         #skosmos_dev = Skosmos.objects.filter(port=9080)
-        #skosmos_dev.add_vocabulary(SkosmosConfig....)
+        # skosmos_dev.add_vocabulary(SkosmosConfig....)
 
     def set_dev(self) -> None:
         """Sets the state to dev
         """
-        if self.state == State.DEV:
-            raise ValueError('Vocabulary is already in development')
-        self.state = State.DEV
-        #remove from skosmos
+        if self.state != State.DEV:
+            self.state = State.DEV
+        # remove from skosmos
         #skosmos_dev = Skosmos.objects.filter(port=9080)
-        #skosmos_dev.delete_vocabulary(self.name)
+        # skosmos_dev.delete_vocabulary(self.name)
 
-    #permission required participant or owner
+    # permission required participant or owner
     def remove_term(self, name: str) -> None:
         """Removes a Term from the Vocabulary and deletes it
 
@@ -155,7 +158,7 @@ class Vocabulary(models.Model):
         self.set_dev_if_live()
         term.delete()
 
-    #permission required participant or owner
+    # permission required participant or owner
     def add_term(self, name: str) -> None:
         """Adds a Term to the Vocabulary
 
@@ -163,9 +166,9 @@ class Vocabulary(models.Model):
             name (str): Name of the Term
         """
         self.term_set.add(Term.models.Term.create(name=name))
-        #record user who added Term as contributor if not already done
+        # record user who added Term as contributor if not already done
 
-    #permission required owner
+    # permission required owner
     def add_profile(self, profile: Profile, permission: str) -> None:
         """Adds a User to the Vocabulary
 
@@ -176,7 +179,7 @@ class Vocabulary(models.Model):
         self.profiles.add(profile)
         assign_perm(permission, profile.user, self)
 
-    #permission required owner
+    # permission required owner
     def add_group(self, group_profile: GroupProfile, permission: str) -> None:
         """Adds a group to the Vocabulary
 
@@ -187,7 +190,7 @@ class Vocabulary(models.Model):
         self.groups.add(group_profile)
         assign_perm(permission, group_profile.group, self)
 
-    #permission required owner
+    # permission required owner
     def remove_profile(self, profile: Profile) -> None:
         """Removes a User from the Vocabulary
 
@@ -195,12 +198,12 @@ class Vocabulary(models.Model):
             profile (Profile): User that gets removed
         """
         self.profiles.remove(profile)
-        #currently removes all permissions user has :(
-        #does this remove permissions given by groups?
+        # currently removes all permissions user has :(
+        # does this remove permissions given by groups?
         for key in get_perms(profile.user, self):
             remove_perm(key, profile.user, self)
 
-    #permission required owner
+    # permission required owner
     def remove_group(self, group_profile: GroupProfile) -> None:
         """Removes a group from the Vocabulary
 
@@ -211,7 +214,7 @@ class Vocabulary(models.Model):
         for key in get_perms(group_profile.group, self):
             remove_perm(key, group_profile.group, self)
 
-    def edit_field(url : str, type : str, content : str) -> None:
+    def edit_field(url: str, type: str, content: str) -> None:
         """Edits a Triple field by using SPARQL Queries and the Fuseki-Dev Instance
 
         Args:
@@ -219,10 +222,10 @@ class Vocabulary(models.Model):
             type (str): Typ of the Triple
             content (str): Content of the Triple
         """
-        #fuseki_dev.buil_sparql_endpoint(self)
+        # fuseki_dev.buil_sparql_endpoint(self)
         placeholder = 123
 
-    def create_field(url : str, type : str, content : str) -> str:
+    def create_field(url: str, type: str, content: str) -> str:
         """Creates a Triple Field on the Fuseki-Dev Instance
 
         Args:
@@ -235,7 +238,7 @@ class Vocabulary(models.Model):
         """
         placeholder = 123
 
-    def delete_field(url : str) -> None:
+    def delete_field(url: str) -> None:
         """Deletes a Triple Field on the Fuseki-Dev Instance
 
         Args:
@@ -243,7 +246,7 @@ class Vocabulary(models.Model):
         """
         placeholder = 123
 
-    def search(input : str):
+    def search(input: str):
         """Searches all Vocabularies for input. Vocabularies that contain input get put into a list and the list gets returned
 
         Args:
--- evoks/vocabularies/views.py
+++ evoks/vocabularies/views.py
@@ -8,14 +8,16 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 from django.contrib.auth.models import User, Group
 from django.shortcuts import redirect, reverse
 from django.core.paginator import Paginator
-from django.core.exceptions import PermissionDenied
+from django.core.exceptions import PermissionDenied, ObjectDoesNotExist
+
 
 def index(request, name):
 
     # TODO fix csrf
     if request.method == 'DELETE':
         # Delete vocabulary
-        Vocabulary.objects.get(name=name).delete()
+        print('DELETETETET')
+        # Vocabulary.objects.get(name=name).delete()
         return HttpResponse(status=204)
 
     print('yeet')
@@ -32,26 +34,33 @@ def index(request, name):
 
 
 def settings(request, name):
-    vocabulary = Vocabulary.objects.get(name=name)
-    #vocabulary = Vocabulary.objects.create(name='abc')
+    try:
+        vocabulary = Vocabulary.objects.get(name=name)
+        print(vocabulary.state)
+    except ObjectDoesNotExist:
+        return redirect('base')
+
     context = {
         'vocabulary': vocabulary
     }
-    if(request.method == 'POST'):
-        if('delete' in request.POST):
-            #vocabulary.delete()
-            return redirect('profile')
+
+
+    if request.method == 'POST':
+        if 'delete' in request.POST:
+            # vocabulary.delete()
+            print('yetetet')
+            return redirect('base')
         elif(request.POST['vocabulary-setting'] == 'Live'):
             vocabulary.set_live()
-        elif(request.POST['vocabulary-setting'] == 'Development'):
+        elif(request.POST['vocabulary-setting'] == 'Review'):
             vocabulary.set_review()
-        elif(request.POST['vocabulary-setting'] == 'Private to you'):
+        elif(request.POST['vocabulary-setting'] == 'Development'):
             vocabulary.set_dev()
-        
     return render(request, 'vocabulary_setting.html', context)
 
 
-def members(request : HttpRequest, name):
+
+def members(request: HttpRequest, name):
     # if request.user.is_authenticated:
     #user = request.user
     vocabulary = Vocabulary.objects.get(name=name)
@@ -60,67 +69,68 @@ def members(request : HttpRequest, name):
     page_number = request.GET.get('page')
     page_obj = p.get_page(page_number)
 
-    #TODO
-    #allow empty list?
-    #does it show all users of group or just groups?
+    # TODO
+    # allow empty list?
+    # does it show all users of group or just groups?
 
     p.allow_empty_first_page
- 
+
     context = {
-        'vocabulary' : vocabulary,
-        'members' : page_obj
+        'vocabulary': vocabulary,
+        'members': page_obj
     }
 
-    if(request.method == 'POST'):
-        if('invite' in request.POST):
-            #still to sensitive should ignore things like whitespace at the end
+    if request.method == 'POST':
+        if 'invite' in request.POST:
+            # still to sensitive should ignore things like whitespace at the end
 
-            #needs permission owner?
-            #if user.has_perm('owner', vocabulary):
+            # needs permission owner?
+            # if user.has_perm('owner', vocabulary):
             invite_str = request.POST['email']
-            #add immediately or send invite email?
-            #right error codes?
+            # add immediately or send invite email?
+            # right error codes?
 
-            #check if User/Group exists
+            # check if User/Group exists
             if User.objects.filter(email=invite_str).exists():
                 invite_user = User.objects.get(email=invite_str)
-                #check if User already on vocabulary
+                # check if User already on vocabulary
                 if not vocabulary.profiles.all().filter(user=invite_user).exists():
                     vocabulary.add_profile(invite_user.profile, 'participant')
                 else:
                     return HttpResponse('User is already part of this vocabulary', status=400)
             elif Group.objects.filter(name=invite_str).exists():
                 invite_group = Group.objects.get(name=invite_str)
-                #check if Group already on vocabulary
+                # check if Group already on vocabulary
                 if not vocabulary.groups.all().filter(group=invite_group).exists():
-                    vocabulary.add_group(invite_group.groupprofile, 'participant')
+                    vocabulary.add_group(
+                        invite_group.groupprofile, 'participant')
                 else:
-                     return HttpResponse('Group is already part of this vocabulary', status=400)
+                    return HttpResponse('Group is already part of this vocabulary', status=400)
             else:
                 return HttpResponse('User/Group does not exist', status=404)
-            #refresh page
+            # refresh page
             return redirect('vocabulary_members', vocabulary.name)
-            #else
-                #raise PermissionDenied
-        elif('kickall' in request.POST):
-            #needs permission owner?
-            #if user.has_perm('owner', vocabulary):
+            # else
+            #raise PermissionDenied
+        elif 'kickall' in request.POST:
+            # needs permission owner?
+            # if user.has_perm('owner', vocabulary):
             for p in vocabulary.profiles.all():
-                #kick creator too?
+                # kick creator too?
                 vocabulary.remove_profile(p)
             return redirect('vocabulary_members', vocabulary.name)
-            #else
-                #raise PermissionDenied
+            # else
+            #raise PermissionDenied
 
-        #for loop ok?
+        # for loop ok?
         for p in vocabulary.profiles.all():
-            #needs permission owner?
-            #if user.has_perm('owner', vocabulary):
+            # needs permission owner?
+            # if user.has_perm('owner', vocabulary):
             if('kick {0}'.format(p.name) in request.POST):
                 vocabulary.remove_profile(p)
                 return redirect('vocabulary_members', vocabulary.name)
 
-            #else
+            # else
                 #raise PermissionDenied
 
     return render(request, 'vocabulary_members.html', context)
