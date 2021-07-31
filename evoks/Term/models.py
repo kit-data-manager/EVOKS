5c7b5d56dded7890c35265fc57e10cb00ebffc56
evoksontologe <84095208+evoksontologe@users.noreply.github.com>
2021-07-31 16:53:32 +0200

    f

--- evoks/Term/models.py
+++ evoks/Term/models.py
@@ -4,7 +4,7 @@ from django.db.models.deletion import CASCADE
 # Create your models here.
 
 class Term(models.Model):
-    name = models.CharField(max_length=30, unique=True)
+    name = models.SlugField(max_length=50, unique=True)
     vocabulary = models.ForeignKey(to='vocabularies.Vocabulary', on_delete=CASCADE, blank=True, null=True)
 
     @classmethod
--- evoks/Term/views.py
+++ evoks/Term/views.py
@@ -2,11 +2,38 @@ from django.http.request import HttpRequest
 from django.shortcuts import render
 from Term.models import Term
 from vocabularies.models import Vocabulary
+from itertools import chain
 
-def term_detail(request : HttpRequest, name : str,term_name : str):
+def term_detail(request : HttpRequest, name : str,term_slug : str):
+    """View for the Term Detail Page
+
+    Args:
+        request (HttpRequest): request object
+        name (str): name of vocabulary term is a part of
+        term_slug (str): slug of the term name
+
+    Returns:
+        HttpResponse: Http Response object
+    """
+
     vocabulary = Vocabulary.objects.get(name=name)
     #TODO turn slug into term name
     #term = Term.objects.get(name=term_name)
+
+    # put comments and tags on vocabulary into a list sorted from newest to oldest
+    #TODO term
+    comments = vocabulary.comment_set.filter()
+    tags = vocabulary.tag_set.filter()
+    activity_list = sorted(
+        chain(comments, tags),
+        key=lambda obj: str(obj.post_date), reverse=True)
+
+    # add type of activity to activity list so we can render it differently
+    for index, key in enumerate(activity_list):
+        activity_list[index].type = key.__class__.__name__
+
+
+
     context = {
         'vocabulary' : vocabulary
         #'term' : term
