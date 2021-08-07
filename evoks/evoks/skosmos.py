067bfe9f4287dfb073eaf490d891bfecd8b9b983
Jonas Scholz <jonas.scholz@bbscholz.de>
2021-08-07 16:05:30 +0200

    improve migration test coverage to 100%

--- evoks/evoks/skosmos.py
+++ evoks/evoks/skosmos.py
@@ -1,6 +1,6 @@
 from Skosmos.skosmos import Skosmos
-from evoks.settings import SKOSMOS_DEV_DIR
+from evoks.settings import SKOSMOS_DEV_DIR, SKOSMOS_LIVE_DIR
 
 skosmos_dev = Skosmos(SKOSMOS_DEV_DIR)
 
-skosmos_live = Skosmos('skosmos-live/config.ttl')
+skosmos_live = Skosmos(SKOSMOS_LIVE_DIR)
--- evoks/tests/migration/test_migration.py
+++ evoks/tests/migration/test_migration.py
@@ -3,6 +3,10 @@ from Fuseki.fuseki import Fuseki
 from vocabularies.models import Vocabulary
 from Migration.migration_context import MigrationContext
 from Migration.backup_migration_strategy import BackupMigrationStrategy
+from time import sleep
+import unittest
+import os
+from evoks.skosmos import skosmos_live
 
 
 class FusekiTestCase(TestCase):
@@ -10,11 +14,34 @@ class FusekiTestCase(TestCase):
     def setUp(self) -> None:
         self.fuseki_dev = Fuseki(
             'fuseki-dev', 3030, 'development', 'fuseki-dev/backup')
-        self.vocabulary = Vocabulary(name='ds')
-        self.vocabulary.save()
-        # self.fuseki_dev.create_vocabulary(self.vocabulary)
+        self.fuseki_live = Fuseki(
+            'fuseki-live', 3030, 'production', 'fuseki-dev/backup')
 
-    def test_get_copy_tasks(self):
+        self.vocabulary = Vocabulary(name='vokabular34')
+        self.vocabulary.save()
+        self.fuseki_dev.create_vocabulary(self.vocabulary)
+        sleep(5)
+        self.delete_vocabularies = [self.vocabulary]
+
+    def tearDown(self) -> None:
+        # remove all backup files
+        for f in os.listdir(self.fuseki_dev.backup_path):
+            os.remove(os.path.join(self.fuseki_dev.backup_path, f))
+        # delete all datasets
+        try:
+            for vocab in self.delete_vocabularies:
+                self.fuseki_dev.delete_vocabulary(vocab)
+        except:
+            pass
+        skosmos_live.delete_vocabulary(self.vocabulary.name)
+
+    def test_migration(self):
         context = MigrationContext(BackupMigrationStrategy())
         context.start(self.vocabulary)
         self.assertTrue(True)
+        self.fuseki_live.delete_vocabulary(self.vocabulary)
+
+    def test_set_strategy(self):
+        context = MigrationContext(None)
+        context.set_migration_strategy(BackupMigrationStrategy())
+        self.assertIsNotNone(context.strategy)
