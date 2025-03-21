15fc2ac1b36700ef1b375aad9b1285b15652ebd6
Christopher Raquet <xf3778@kit.edu>
2025-03-21 16:48:22 +0100

    Add http basic authentication to metrics endpoint

--- evoks/evoks/middleware.py
+++ evoks/evoks/middleware.py
@@ -1,14 +1,18 @@
 import hashlib
+import base64
+import os
 
-from django.http.response import HttpResponseNotFound
+from django.http import HttpRequest
+from django.http.response import HttpResponseNotFound, HttpResponseForbidden
 from django.utils.deprecation import MiddlewareMixin
 from prometheus_client import Counter, Gauge
+
 from vocabularies.models import Vocabulary
 from django.shortcuts import redirect
 
 MONITORING_EXEMPT_URLS = ['metrics/', 'health/']
 LOGIN_EXEMPT_URLS = ['logout', 'signup', 'login', 'reset_password/', 'health/'
-                     'reset_password_sent/', 'reset_password_complete/', 'ToS', 'metrics/']
+                     'reset_password_sent/', 'reset_password_complete/', 'ToS']
 
 users_total_counter = Counter("evoks_users_total", "Total number of requests served")
 users_unique_gauge = Gauge("evoks_unique_users", "Unique number of users, distinguished by IP")
@@ -38,9 +42,10 @@ class MonitoringMiddleware(MiddlewareMixin):
                 users_unique_gauge.set(len(self.stored_ips))
 
 
-class LoginRequiredMiddleware:
+class LoginRequiredMiddleware(MiddlewareMixin):
 
     def __init__(self, get_response):
+        super().__init__(get_response)
         self.get_response = get_response
 
     def __call__(self, request):
@@ -48,11 +53,30 @@ class LoginRequiredMiddleware:
         return response
 
     @staticmethod
-    def process_view(request, view_func, view_args, view_kwargs):
+    def basic_auth(request: HttpRequest):
+        if 'HTTP_AUTHORIZATION' in request.META:
+            auth = request.META['HTTP_AUTHORIZATION'].split()
+            if len(auth) == 2:
+                if auth[0].lower() == "basic":
+                    uname, passwd = base64.b64decode(auth[1]).split(b':')
+                    req_uname = os.environ.get('METRICS_USER', 'metrics')
+                    req_passwd = os.environ.get('METRICS_PASS', 'metricsPass')
+                    if uname.decode() == req_uname and passwd.decode() == req_passwd:
+                        return True
+        return False
+
+    @staticmethod
+    def process_view(request: HttpRequest, view_func, view_args, view_kwargs):
         assert hasattr(request, 'user')
         path = request.path_info.lstrip('/')
         if request.path.startswith('/reset/'):
             return None
+
+        if path == "metrics/":
+            if LoginRequiredMiddleware.basic_auth(request):
+                return None
+            else:
+                return HttpResponseForbidden()
 
         if not request.user.is_authenticated:
             if not any(url == path for url in LOGIN_EXEMPT_URLS):
